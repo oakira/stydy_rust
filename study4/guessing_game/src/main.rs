38f898118ee5339aa5d3a461b67c74f69494bf23
akira ogawa <oakira@outlook.com>
2020-02-19 16:46:32 +0900

    stydy4 fixed

--- study4/guessing_game/src/main.rs
+++ study4/guessing_game/src/main.rs
@@ -1,3 +1,29 @@
+extern crate rand;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-	println!("Hello, world!");
+	println!("Guess the number!");
+	let secret_number = rand::thread_rng().gen_range(1, 101);
+	loop {
+		println!("please input your guess.");
+		let mut guess = String::new();
+		io::stdin()
+			.read_line(&mut guess)
+			.expect("failed to read line");
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
+		println!("you guessed: {}", guess);
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("too small!"),
+			Ordering::Greater => println!("too big!"),
+			Ordering::Equal => {
+				println!("you win!!");
+				break;
+			}
+		}
+	}
 }
